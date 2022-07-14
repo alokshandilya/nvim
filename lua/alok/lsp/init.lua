10a6051a03fe88f6d79618751a620a1862928c57
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-15 02:32:21 +0530

    lsp initial commit

---
+++ lua/alok/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "alok.lsp.configs"
+require("alok.lsp.handlers").setup()
+require "alok.lsp.null-ls"
