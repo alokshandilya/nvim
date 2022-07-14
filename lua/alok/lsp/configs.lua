c1e20adf5c085562e5d4e2f80331f440fe791122
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-15 02:35:56 +0530

    fix path user

--- lua/alok/lsp/configs.lua
+++ lua/alok/lsp/configs.lua
@@ -18,10 +18,10 @@ lsp_installer.setup({
 
 for _, server in pairs(servers) do
   local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
+    on_attach = require("alok.lsp.handlers").on_attach,
+    capabilities = require("alok.lsp.handlers").capabilities,
   }
-  local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+  local has_custom_opts, server_custom_opts = pcall(require, "alok.lsp.settings." .. server)
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
   end
