813f16c0c9aabc38bc45486f808b6289aec7d159
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-20 23:26:19 +0530

    add autocommand to build pdf of notes
    - uses buildNote script [pandoc]

--- lua/alok/autocommands.lua
+++ lua/alok/autocommands.lua
@@ -28,6 +28,11 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  augroup _notes
+    autocmd!
+    autocmd BufWritePost *note-*.md silent !buildNote %:p
+  augroup end
 ]]
 
 -- Autoformat
