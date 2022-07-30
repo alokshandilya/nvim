abcbcf7a77894e06d9ec90b54ce3cdbec4f8307d
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-30 12:26:25 +0530

    lua style, fix typo

--- lua/alok/colorscheme.lua
+++ lua/alok/colorscheme.lua
@@ -1,22 +1,18 @@
--- -- #######################################
--- -- ########### gruvbox-material ##########
--- -- #######################################
-vim.cmd [[
-try
-  set background=dark
-  let g:gruvbox_material_better_performance=1
-  let g:gruvbox_material_background = 'hard' " soft, medium(default), hard
-  let g:gruvbox_material_foreground = 'material' " material(default), mix, original
-  let g:gruvbox_material_enable_bold=1
-  let g:gruvbox_material_enable_italic=1
-  let g:gruvbox_material_transparent_background=1
-  colorscheme gruvbox-material
-endtry
-]]
+-- #######################################
+-- ########### gruvbox-material ##########
+-- #######################################
+vim.opt.background="dark"
+vim.g.gruvbox_material_better_performance=1
+vim.g.gruvbox_material_background = 'hard' -- soft, medium(default), hard
+vim.g.gruvbox_material_foreground = 'material' -- material(default), mix, original
+vim.g.gruvbox_material_enable_bold=1
+vim.g.gruvbox_material_enable_italic=1
+vim.g.gruvbox_material_transparent_background=1
+vim.cmd[[colorscheme gruvbox-material]]
 
--- -- #######################################
--- -- ########### gruvbox-material ##########
--- -- #######################################
+-- #################################
+-- ########### tokyonight ##########
+-- #################################
 -- vim.g.tokyonight_style = "night"
 -- vim.g.tokyonight_transparent = true
 -- vim.g.tokyonight_hide_inactive_statusline = true
