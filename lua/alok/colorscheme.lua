047a01eaf8cd3cef2a4130362a7a4b2ab0006dac
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-03 13:42:16 +0530

    remove tokyonight

--- lua/alok/colorscheme.lua
+++ lua/alok/colorscheme.lua
@@ -9,15 +9,3 @@ vim.g.gruvbox_material_enable_bold=1
 vim.g.gruvbox_material_enable_italic=1
 vim.g.gruvbox_material_transparent_background=1
 vim.cmd[[colorscheme gruvbox-material]]
-
--- #################################
--- ########### tokyonight ##########
--- #################################
--- vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_transparent = true
--- vim.g.tokyonight_hide_inactive_statusline = true
--- vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_dark_float = false
--- vim.g.tokyonight_dark_sidebar = false
--- vim.g.tokyonight_lualine_bold = true
--- vim.cmd[[colorscheme tokyonight]]
