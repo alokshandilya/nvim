742f38e302b9f57d0ee5a0e34f1875e053a85968
alokshandilya <alok.shandilya10@gmail.com>
2024-08-14 05:18:12 +0530

    add lualine plugin

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ require("aloks.keymaps")
 require("aloks.lazy")
 
 -- plugins
-require("plugins.gruvbox")
+require("plugins.gruvbox") -- for gruvbox configuration
+require("plugins.lualine") -- for lualine configuration
+
 require("plugins.keymaps") -- for plugins related keymaps
 require("plugins.options") -- for plugins related options
