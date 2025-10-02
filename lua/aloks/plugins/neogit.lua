0595eedb8c1de78d7917aeb532567f9859c3c9c3
alokshandilya <alok.shandilya10@gmail.com>
2025-10-02 09:18:56 +0530

    feat: Refactor neogit plugin config and fix typo in dependencies

--- lua/aloks/plugins/neogit.lua
+++ lua/aloks/plugins/neogit.lua
@@ -1,30 +1,28 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim", -- required
+        "sindrets/diffview.nvim", -- optional - Diff integration
 
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
-  },
-  keys = {
-    {
-      "<leader>ng",
-      "<cmd>Neogit kind=vsplit<cr>",
-      mode = "n",
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",          -- optional
     },
-    {
-      "<leader>ngc",
-      "<cmd>Neogit commit<cr>",
-      mode = "n",
+    keys = {
+        {
+            "<leader>ng",
+            "<cmd>Neogit kind=vsplit<cr>",
+            mode = "n",
+        },
+        {
+            "<leader>ngc",
+            "<cmd>Neogit commit<cr>",
+            mode = "n",
+        },
     },
-  },
-  dependcies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
-    "ibhagwan/fzf-lua",
-  },
-  config = true,
+    config = function()
+        require("neogit").setup({
+            graph_style = "kitty",
+        })
+    end,
 }
